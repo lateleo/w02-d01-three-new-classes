9bb7d40469e038480d61c35d9e904e2831c897cf
Art Burtch <artburtch@huskers.unl.edu>
2016-03-22 09:26:19 -0500

    done

---
+++ car.rb
@@ -0,0 +1,37 @@
+#require "pry"
+
+class Car
+
+  def initialize(options={})
+    @make = options[:make]
+    @model = options[:model]
+    @year = options[:year]
+    @color = options[:color]
+    @plate_number = options[:plate_number]
+  end
+
+  def make
+    @make.dup
+  end
+
+  def model
+    @model.dup
+  end
+
+  def year
+    @year.dup
+  end
+
+  def color
+    @color.dup
+  end
+
+  def plate_number
+    @plate_number.dup
+  end
+
+  def plate_number=(new_plate_number)
+    @plate_number = new_plate_number
+  end
+
+end  
---
+++ person.rb
@@ -0,0 +1,43 @@
+class Person
+
+  def initialize(options={})
+    @name = options[:name]
+    @birthday = options[:birthday]
+    @ethnicity = options[:ethnicity]
+    @gender = options[:gender]
+    @address = options[:address]
+  end
+
+  def name
+    @name.dup
+  end
+
+  def name=(new_name)
+    @name = new_name
+  end
+
+  def birthday
+    @birthday.dup
+  end
+
+  def ethnicity
+    @ethnicity.dup
+  end
+
+  def gender
+    @gender.dup
+  end
+
+  def gender=(new_gender)
+    @gender = new_gender
+  end
+
+  def address
+    @address.dup
+  end
+
+  def address=(new_address)
+    @address = new_address
+  end
+
+end
